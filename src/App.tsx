6be442bc38b89f75c4b963b641201f60a78aacc9
Vu Thanh Dat <dev.thanhdat@gmail.com>
2022-07-13 14:43:34 +0700

    [WIP] Add jquery and stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import ExampleComponent from "./components/example.component";
 
 const App = () => {
   return (
-    <div className="App">
-      Hello world
-    </div>
+      <ExampleComponent/>
   );
 }
 
---
+++ src/components/example.component.tsx
@@ -0,0 +1,9 @@
+import {FC} from 'react';
+
+const ExampleComponent: FC<any> = () => {
+    return <div>
+        test
+    </div>;
+}
+
+export default ExampleComponent;
