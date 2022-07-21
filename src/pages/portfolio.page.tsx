f2317c37c71ef1df409d651c18223ce05b7d7939
Vu Thanh Dat <dev.thanhdat@gmail.com>
2022-07-21 22:27:41 +0700

    [Feature] Update personal information

--- src/pages/portfolio.page.tsx
+++ src/pages/portfolio.page.tsx
@@ -6,6 +6,8 @@ import {ExperienceComponent} from "../components/experience.component";
 import {workData} from "../data/work.data";
 import {EducationComponent} from "../components/education.component";
 import {educationData} from "../data/education.data";
+import {SkillComponent} from "../components/skill.component";
+import {AwardComponent} from "../components/award.component";
 
 const PortfolioPage: FC<any> = () => {
     const firstName = 'Dat '
@@ -13,10 +15,19 @@ const PortfolioPage: FC<any> = () => {
     const address = `Saigon · Vietnam · (+84) 94-347-6316 ·`
     const email = `dev.thanhdat@gmail.com`
     const shortIntroduction = `I am passionate software wizard with 6 years experience of Java, AWS, NodeJS`
-    const githubUrl = '';
-    const linkedInUrl = '';
-    const twitterUrl = '';
-    const facebookUrl = '';
+    const githubUrl = 'https://github.com/vuthanhdatdev';
+    const linkedInUrl = 'https://www.linkedin.com/in/vuthanhdat';
+    const twitterUrl = 'https://twitter.com/vuthanhdat_dev';
+    const facebookUrl = 'https://www.facebook.com/vuthanhdat.developer';
+    const shortBriefLife = `
+    Apart from being a web developer,
+     I enjoy most of my time being outdoors.
+      In the winter, I am an avid skier and novice ice climber.
+       During the free time in my life, I enjoy mechanical keyboard building.`;
+    const shortBriefLife2 = `When forced indoors,
+     I follow a number of sci-fi and fantasy genre movies and television shows,
+      I am an aspiring chef,
+       and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.`;
     return <Fragment>
         <NavbarComponent
             firstName={firstName}
@@ -39,99 +50,17 @@ const PortfolioPage: FC<any> = () => {
             <hr className="m-0" />
             <EducationComponent data={educationData}/>
             <hr className="m-0" />
-            <section className="resume-section" id="skills">
-                <div className="resume-section-content">
-                    <h2 className="mb-5">Skills</h2>
-                    <div className="subheading mb-3">Programming Languages & Tools</div>
-                    <ul className="list-inline dev-icons">
-                        <li className="list-inline-item"><i className="fab fa-html5"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-css3-alt"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-js-square"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-angular"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-react"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-node-js"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-sass"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-less"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-wordpress"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-gulp"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-grunt"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-npm"></i></li>
-                    </ul>
-                    <div className="subheading mb-3">Workflow</div>
-                    <ul className="fa-ul mb-0">
-                        <li>
-                            <span className="fa-li"><i className="fas fa-check"></i></span>
-                            Mobile-First, Responsive Design
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-check"></i></span>
-                            Cross Browser Testing & Debugging
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-check"></i></span>
-                            Cross Functional Teams
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-check"></i></span>
-                            Agile Development & Scrum
-                        </li>
-                    </ul>
-                </div>
-            </section>
+            <SkillComponent workFlows={[]}/>
             <hr className="m-0" />
             <section className="resume-section" id="interests">
                 <div className="resume-section-content">
                     <h2 className="mb-5">Interests</h2>
-                    <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
-                    <p className="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+                    {/*<p>{shortBriefLife}</p>*/}
+                    {/*<p className="mb-0">{shortBriefLife2}</p>*/}
                 </div>
             </section>
             <hr className="m-0" />
-            <section className="resume-section" id="awards">
-                <div className="resume-section-content">
-                    <h2 className="mb-5">Awards & Certifications</h2>
-                    <ul className="fa-ul mb-0">
-                        <li>
-                            <span className="fa-li"><i className="fas fa-trophy text-warning"></i></span>
-                            Google Analytics Certified Developer
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-trophy text-warning"></i></span>
-                            Mobile Web Specialist - Google Certification
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - University of Colorado Boulder - Emerging Tech Competition 2009
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-trophy text-warning"></i></span>
-                            2
-                            <sup>nd</sup>
-                            Place - University of Colorado Boulder - Emerging Tech Competition 2008
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - James Buchanan High School - Hackathon 2006
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-trophy text-warning"></i></span>
-                            3
-                            <sup>rd</sup>
-                            Place - James Buchanan High School - Hackathon 2005
-                        </li>
-                    </ul>
-                </div>
-            </section>
+            <AwardComponent awards={[]} />
         </div>
     </Fragment>
 }
