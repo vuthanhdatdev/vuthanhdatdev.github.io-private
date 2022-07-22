0fc00cd5a951b79d16e56b3e45e8cfe065dc7cd6
Vu Thanh Dat <dev.thanhdat@gmail.com>
2022-07-22 17:07:26 +0700

    [Improvement] Improve UI UX when click to button

--- src/components/skill.component.tsx
+++ src/components/skill.component.tsx
@@ -10,7 +10,6 @@ export interface SkillComponentProps {
 }
 
 export const SkillComponent: FC<SkillComponentProps> = (props: SkillComponentProps) => {
-    const { workFlows } = props;
     return <section className="resume-section" id="skills">
         <div className="resume-section-content">
             <h2 className="mb-5">Skills</h2>
--- src/data/education.data.ts
+++ src/data/education.data.ts
@@ -1,10 +1,18 @@
 export interface EducationDataProps {
     school: string;
-    gpa: string;
+    gpa?: string;
     startFrom: string;
     endFrom?: string;
     degreeType: string;
     major?: string;
+}
+
+export interface CertificationDataProps {
+    name: string;
+    startFrom?: string;
+    endFrom?: string;
+    score?: string;
+    providedBy: string;
 }
 
 export const educationData: EducationDataProps[] = [
@@ -16,4 +24,24 @@ export const educationData: EducationDataProps[] = [
         degreeType: 'Bachelor',
         major: 'Software Engineering'
     },
+];
+
+export const certificationData: CertificationDataProps[] = [
+    {
+        startFrom: 'May 2022',
+        endFrom: 'May 2025',
+        name: 'AWS Solution Architect Associate',
+        providedBy: 'Amazon'
+    },
+    {
+        name: 'Oracle Certified Professional, Java SE 8 Programmer',
+        providedBy: 'Oracle',
+    },
+    {
+        name: 'IELTS',
+        score: '7.0',
+        startFrom: 'August 2018',
+        endFrom: 'August 2020',
+        providedBy: 'British Council'
+    }
 ];
