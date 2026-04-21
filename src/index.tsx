ec5815da58fc25244ca8e2e88fc3539d31a8cbf4
Joe <joe.vuthanh.ta@mantelgroup.vn>
2026-04-21 13:50:30 +0700

    feat: rework CSS (#10)
    * feat: rework CSS
    
    * add GHA deployment

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+config.autoAddCss = false
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
